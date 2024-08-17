af3bd6537a402b5bbdc9302f9c75c3757dd55617
TomPlr <thomas.peltre@pm.me>
2024-08-17 17:32:29 +0200

    feat(profile-page): improve profile page

--- src/main/java/com/paymybuddy/mapper/UserMapper.java
+++ src/main/java/com/paymybuddy/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package com.paymybuddy.mapper;
 
+import com.paymybuddy.dto.ProfileFormDto;
 import com.paymybuddy.dto.UserDto;
 import com.paymybuddy.entity.UserEntity;
+import com.paymybuddy.model.UserModel;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -10,4 +12,8 @@ public interface UserMapper {
     UserDto toDto(UserEntity userEntity);
 
     UserEntity toEntity(UserDto userDto);
+
+    UserDto toUserDto(ProfileFormDto profileFormDto);
+
+    ProfileFormDto toProfileFormDto(UserModel userModel);
 }
